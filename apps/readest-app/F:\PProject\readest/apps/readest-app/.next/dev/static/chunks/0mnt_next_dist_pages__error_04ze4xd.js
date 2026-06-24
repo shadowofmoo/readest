1a741eb9eb0331932dc176fb94d4f3f05a420a65
Super User <root@DESKTOP-DAVFHSU.localdomain>
2026-06-24 17:42:08 +0800

    fix: remove gstack submodule from git index

---
+++ apps/readest-app/F:\PProject\readest/apps/readest-app/.next/dev/static/chunks/0mnt_next_dist_pages__error_04ze4xd.js
@@ -0,0 +1,8 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/node_modules/.pnpm/next@16.2.6_@babel+core@7.2_752a3439d623467d2f7bf27bb43cbcf9/node_modules/next/dist/pages/_error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/node_modules/.pnpm/next@16.2.6_@babel+core@7.2_752a3439d623467d2f7bf27bb43cbcf9/node_modules/next/dist/pages/_error.js', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+]);
---
+++ apps/readest-app/F:\PProject\readest/apps/readest-app/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/0ke9_next_dist_compiled_09-bm4.._.js",
+  "static/chunks/0ke9_next_dist_shared_lib_0pl~ye3._.js",
+  "static/chunks/0ke9_next_dist_client_09a7unt._.js",
+  "static/chunks/0ke9_next_dist_0pxg-ml._.js",
+  "static/chunks/0mnt_next_dist_pages__error_04ze4xd.js",
+  "static/chunks/[next]_entry_page-loader_ts_0b-zj01._.js",
+  "static/chunks/0e-u_react-dom_0037m-m._.js",
+  "static/chunks/node_modules__pnpm_0byoz11._.js",
+  "static/chunks/[root-of-the-server]__0frfgkm._.js",
+  "static/chunks/[project]_apps_readest-app_src_pages__error_07xvfw~._.js",
+  "static/chunks/turbopack-[project]_apps_readest-app_src_pages__error_0iib8zh._.js"
+])
